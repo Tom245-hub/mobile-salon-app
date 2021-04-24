b39deb5260905aae88469e22b3db7449e6f37d90
Tomasz Oszkiel <tomaszoszkiel09@gmail.com>
2021-04-24 23:39:26 +0200

    fix bug Webpack

--- frontend/webpack/development.js
+++ frontend/webpack/development.js
@@ -1,47 +1,46 @@
 module.exports = {
   devServer: {
-    contentBase: './public',
+    contentBase: "./public",
     port: 3000,
     historyApiFallback: true,
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.module\.s(a|c)ss$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: {
-                localIdentName: '[local]',
+                localIdentName: "[local]",
               },
             },
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true,
-            }
-          }
-
-        ]
+            },
+          },
+        ],
       },
       {
         test: /\.(s(a|c)ss|css)$/,
         exclude: /\.module.(s(a|c)ss)$/,
-        loader: [
-          'style-loader',
-          'css-loader',
+        use: [
+          "style-loader",
+          "css-loader",
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true,
-            }
-          }
-        ]
+            },
+          },
+        ],
       },
-    ]
+    ],
   },
 };
