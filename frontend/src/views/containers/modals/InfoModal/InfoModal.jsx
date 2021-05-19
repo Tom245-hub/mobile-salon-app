2703e51b361021167f6aa9b26914e0e1f09efbc6
Tomasz Oszkiel <tomaszoszkiel09@gmail.com>
2021-05-19 10:59:08 +0200

    InfoModal: propTypes

--- frontend/src/views/containers/modals/InfoModal/InfoModal.jsx
+++ frontend/src/views/containers/modals/InfoModal/InfoModal.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Modal } from "react-bootstrap";
 
-const InfoModal = (props) => {
+const InfoModal = ({ infoModal, setInfoModal, titleText, bodyText }) => {
   return (
-    <Modal size='lg' show={props.infoModal} onHide={() => props.setInfoModal(false)} aria-labelledby='confirm-modal'>
+    <Modal size='lg' show={infoModal} onHide={() => setInfoModal(false)} aria-labelledby='confirm-modal'>
       <Modal.Header closeButton>
-        <Modal.Title id='confirm-modal'>{props.titleText}</Modal.Title>
+        <Modal.Title id='confirm-modal'>{titleText}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>{props.bodyText}</Modal.Body>
+      <Modal.Body>{bodyText}</Modal.Body>
     </Modal>
   );
+};
+
+InfoModal.propTypes = {
+  infoModal: PropTypes.bool.isRequired,
+  setInfoModal: PropTypes.func.isRequired,
+  titleText: PropTypes.string.isRequired,
+  bodyText: PropTypes.string.isRequired,
 };
 
 export default InfoModal;
