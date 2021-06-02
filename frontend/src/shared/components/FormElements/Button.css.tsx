26b60a484391ffe3f0f4fbf790a540462699a891
Tomasz Oszkiel <tomaszoszkiel09@gmail.com>
2021-06-02 17:29:19 +0200

    Button: variants

--- frontend/src/shared/components/FormElements/Button.css.tsx
+++ frontend/src/shared/components/FormElements/Button.css.tsx
@@ -1,21 +1,50 @@
 import styled from "styled-components";
 
-interface StyledButtonProps {
+interface StyledRootButtonProps {
   margin?: string;
 }
 
-export const StyledButton = styled.button<StyledButtonProps>`
+const RootButton = styled.button<StyledRootButtonProps>`
   font-family: ${({ theme }) => theme.fonts.secondary};
   font-size: 1.4rem;
   weight: 400;
-  color: ${({ theme }) => theme.colors.gray.light};
-  background-color: ${({ theme }) => theme.colors.secondary.normal};
-  border: none;
-  border-radius: 5px;
   padding: 0.5rem 1.5rem;
   margin: ${({ margin }) => margin};
   transition: 1s ease-in-out;
   cursor: pointer;
+  border: none;
+  border-radius: 5px;
+`;
+
+export const StyledConfirmButton = styled(RootButton)`
+  color: ${({ theme }) => theme.colors.gray.light};
+  background-color: ${({ theme }) => theme.colors.secondary.normal};
+  &:hover {
+    color: ${({ theme }) => theme.colors.gray.light};
+    background-color: ${({ theme }) => theme.colors.secondary.dark};
+    background-image: radial-gradient(
+      ${({ theme }) => theme.colors.secondary.normal},
+      ${({ theme }) => theme.colors.secondary.dark}
+    );
+  }
+`;
+
+export const StyledCancelButton = styled(RootButton)`
+  color: ${({ theme }) => theme.colors.primary.dark};
+  background-color: ${({ theme }) => theme.colors.gray.light};
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary.dark};
+    background-color: ${({ theme }) => theme.colors.gray.normal};
+    background-image: radial-gradient(
+      ${({ theme }) => theme.colors.gray.normal},
+      ${({ theme }) => theme.colors.gray.dark}
+    );
+  }
+`;
+
+export const StyledButton = styled(RootButton)`
+  color: ${({ theme }) => theme.colors.gray.light};
+  background-color: ${({ theme }) => theme.colors.secondary.normal};
   &:hover {
     color: ${({ theme }) => theme.colors.gray.light};
     background-color: ${({ theme }) => theme.colors.secondary.dark};
--- frontend/src/shared/components/FormElements/Button.tsx
+++ frontend/src/shared/components/FormElements/Button.tsx
@@ -1,19 +1,41 @@
 import React from "react";
-import { StyledButton } from "./Button.css";
+import { Link } from "react-router-dom";
+import { StyledButton, StyledConfirmButton, StyledCancelButton } from "./Button.css";
 
 interface ButtonProps {
   children: string;
+  variant?: "confirm" | "cancel";
+  link?: string;
   type?: "submit" | "reset" | "button";
   onClick?: () => void;
   margin?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, type, onClick, margin }) => {
-  return (
-    <StyledButton type={type} onClick={onClick} margin={margin}>
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant,
+  link,
+  onClick,
+  type,
+  margin,
+}) => {
+  const VariantButton = (() => {
+    switch (variant) {
+      case "confirm":
+        return StyledConfirmButton;
+      case "cancel":
+        return StyledCancelButton;
+      default:
+        return StyledConfirmButton;
+    }
+  })();
+  const button = (
+    <VariantButton onClick={onClick} type={type} margin={margin}>
       {children}
-    </StyledButton>
+    </VariantButton>
   );
+  const content = link ? <Link to={link}>{button}</Link> : button;
+  return <> {content} </>;
 };
 
 export default Button;
