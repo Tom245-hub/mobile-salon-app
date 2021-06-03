a7196d61f3bae927eba7242b238fbeb287ae8c50
Tomasz Oszkiel <tomaszoszkiel09@gmail.com>
2021-06-03 19:15:37 +0200

    DrawerMenu: submenu structure, styling

--- frontend/src/shared/components/Navigation/DrawerMenu.css.tsx
+++ frontend/src/shared/components/Navigation/DrawerMenu.css.tsx
@@ -4,6 +4,14 @@ interface StyledDrawerMenuProps {
   isEnterSlide: boolean;
 }
 
+interface StyledLinkProps {
+  margin: string;
+}
+
+interface StyledNavLinkProps {
+  rotate?: boolean;
+}
+
 export const StyledDrawerMenu = styled.div<StyledDrawerMenuProps>`
   position: absolute;
   top: 0;
@@ -11,7 +19,71 @@ export const StyledDrawerMenu = styled.div<StyledDrawerMenuProps>`
   width: 400px;
   height: 100%;
   background: ${({ theme }) => theme.colors.gray.light};
-  padding: 1rem;
+  display: flex;
+  flex-direction: column;
   z-index: 25;
   transition: 0.5s ease-in-out;
+`;
+
+export const StyledHeader = styled.header`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray.normal};
+  padding: 0.5rem 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StyledSection = styled.section`
+  padding: 1rem;
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StyledFooter = styled.footer`
+  border-top: 1px solid ${({ theme }) => theme.colors.gray.normal};
+  padding: 0.5rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const StyledLogoLink = styled.a`
+  height: 3rem;
+  display: flex;
+  img {
+    height: 100%;
+  }
+`;
+
+export const StyledLink = styled.a<StyledLinkProps>`
+  cursor: pointer;
+
+  svg {
+    margin: ${({ margin }) => margin};
+  }
+`;
+
+export const StyledNavLink = styled.a<StyledNavLinkProps>`
+  font-size: 1.4rem;
+  display: flex;
+  justify-content: space-between;
+
+  svg {
+    color: red;
+    transition: 0.5s ease-in-out;
+    // transform: rotate(180deg);
+
+    transform: ${({ rotate }) => rotate && "rotate(180deg)"};
+  }
+`;
+
+export const StyledMenu = styled.ul`
+  width: 100%;
+`;
+
+export const StyledSubmenu = styled.ul`
+  font-size: 1rem;
+  margin-left: 1rem;
 `;
--- frontend/src/shared/components/Navigation/DrawerMenu.tsx
+++ frontend/src/shared/components/Navigation/DrawerMenu.tsx
@@ -1,48 +1,114 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { StyledDrawerMenu } from "./DrawerMenu.css";
+import {
+  faChevronDown,
+  faMapMarkerAlt,
+  faTimes,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  StyledDrawerMenu,
+  StyledHeader,
+  StyledSection,
+  StyledFooter,
+  StyledLogoLink,
+  StyledLink,
+  StyledNavLink,
+  StyledMenu,
+  StyledSubmenu,
+} from "./DrawerMenu.css";
+import Button from "../FormElements/Button";
 
 interface DrawerMenuProps {
   isEnterSlide: boolean;
-  toggleOpenDrawerMenu: () => void;
+  toggleOpenPortal: () => void;
 }
 
-const DrawerMenu: React.FC<DrawerMenuProps> = ({
-  isEnterSlide,
-  toggleOpenDrawerMenu,
-}) => {
+const DrawerMenu: React.FC<DrawerMenuProps> = ({ isEnterSlide, toggleOpenPortal }) => {
   const content = (
     <StyledDrawerMenu isEnterSlide={isEnterSlide}>
-      <a onClick={toggleOpenDrawerMenu}>
-        <FontAwesomeIcon icon={faTimes} />
-      </a>
+      <StyledHeader>
+        <StyledLogoLink href='/'>
+          <img src='/img/logotyp_symbol_white.png' />
+        </StyledLogoLink>
 
-      <ul>
-        <li>
-          <NavLink to='/uslugi'>
-            usługi <FontAwesomeIcon icon={faChevronDown} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/stylistki'>
-            stylistki <FontAwesomeIcon icon={faChevronDown} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/blog'>
-            blog <FontAwesomeIcon icon={faChevronDown} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/kontakt'>
-            kontakt <FontAwesomeIcon icon={faChevronDown} />
-          </NavLink>
-        </li>
-      </ul>
+        <div>
+          <StyledLink margin='0 0 0 0.5rem'>
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+          </StyledLink>
+          <StyledLink margin='0 0 0 0.5rem'>
+            <FontAwesomeIcon icon={faTimes} />
+          </StyledLink>
+        </div>
+      </StyledHeader>
+
+      <StyledSection>
+        <StyledMenu>
+          <li>
+            <StyledNavLink href='/uslugi' rotate={true}>
+              usługi <FontAwesomeIcon icon={faChevronDown} />
+            </StyledNavLink>
+
+            <StyledSubmenu>
+              <li>
+                <Link to='/uslugi/fryzury-slubne'>fryzury ślubne</Link>
+              </li>
+              <li>
+                <Link to='/uslugi/makijaze-slubne'>makijaże ślubne</Link>
+              </li>
+              <li>
+                <Link to='/uslugi/fryzury-okazyjne'>fryzury okazyjne</Link>
+              </li>
+              <li>
+                <Link to='/uslugi/makijaze-okazyjne'>makijaże okazyjne</Link>
+              </li>
+            </StyledSubmenu>
+          </li>
+          <li>
+            <StyledNavLink href='/stylistki'>
+              stylistki <FontAwesomeIcon icon={faChevronDown} />
+            </StyledNavLink>
+            <StyledSubmenu>
+              <li>
+                <Link to='/'>warszawa</Link>
+              </li>
+              <li>
+                <Link to='/'>kraków</Link>
+              </li>
+              <li>
+                <Link to='/'>wrocław</Link>
+              </li>
+              <li>
+                <Link to='/'>poznań</Link>
+              </li>
+              <li>
+                <Link to='/'>gdańsk</Link>
+              </li>
+            </StyledSubmenu>
+          </li>
+          <li>
+            <StyledNavLink href='/blog'>
+              blog <FontAwesomeIcon icon={faChevronDown} />
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink href='/kontakt'>
+              kontakt <FontAwesomeIcon icon={faChevronDown} />
+            </StyledNavLink>
+          </li>
+        </StyledMenu>
+      </StyledSection>
+
+      <StyledFooter>
+        <StyledLink margin='0 0.5rem 0 0'>
+          <FontAwesomeIcon icon={faUser} />
+          ZALOGUJ
+        </StyledLink>
+
+        <Button link='/strefa-klientki/zamowienie'>ZAREZERWUJ</Button>
+      </StyledFooter>
     </StyledDrawerMenu>
   );
 
